4dacb83e3d466de83774a57ee15c6aefd38d6b65
BaseLeader1 <roeezlevi@gmail.com>
2024-08-06 18:38:03 +0300

    backend with soft delete 2.0

---
+++ restaurant-frontend/src/app/app.config.ts
@@ -0,0 +1,19 @@
+import { ApplicationConfig, InjectionToken } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+
+import { routes } from './app.routes';
+
+export const APPICATION_TITLE = new InjectionToken<string>(
+  'title of application'
+);
+export const BASE_SERVER_URL = new InjectionToken<string>('base url of server');
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    { provide: APPICATION_TITLE, useValue: 'restaurant stock' },
+    { provide: BASE_SERVER_URL, useValue: 'http://localhost:3000/api' },
+    provideHttpClient(),
+    provideRouter(routes),
+  ],
+};
